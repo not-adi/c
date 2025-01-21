585f3633cdcedad2ae0513fdb5b6cc47da8c2252
not-adi <yadav.adi0718@gmail.com>
2025-01-21 17:35:05 +0530

    Added Tower of Hanoi

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # c
-List of important programmes in C Language
+<h1>List of important programmes in C Language</h1>
+<br>  <hr>
+Codes Include - <br>
+Tower Of Hanoi
---
+++ towerofhanoi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void hanoi(int n, char rodfrom, char rodmiddle, char rodto) {
+        if (n == 1) {
+            printf("\n Move disk 1 from rod %c to rod %c", rodfrom, rodto);
+            return;
+        } else {
+        hanoi(n-1,rodfrom,rodto,rodmiddle);
+        printf("\n Move disk %d from rod %c to rod %c", n, rodfrom, rodto);
+        hanoi(n-1,rodmiddle,rodfrom,rodto);
+    }
+}
+
+int main() {
+hanoi(3, 'A', 'B', 'C');
+return 0; }
